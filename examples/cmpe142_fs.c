1e08b913f267a4713a3152232f5ee9b41675c41d
sakshikalra <sakshi38895@gmail.com>
2013-04-14 09:07:54 +0000

    Updated comments

--- examples/cmpe142_fs.c
+++ examples/cmpe142_fs.c
@@ -1,16 +1,13 @@
 /* 
- *  procfs3.c -  create a "file" in /proc, use the file_operation way
- *  		to manage the file.
+ *  cmpe142_fs.c -  Register and create a user file system
  */
  
-#include <linux/kernel.h>	/* We're doing kernel work */
+#include <linux/kernel.h>	/* required for kernel modules */
 #include <linux/module.h>	/* Specifically, a module */
-#include <linux/init.h>	/* Necessary because we use proc fs */
-#include <linux/fs.h>	/* for copy_*_user */
+#include <linux/init.h>	/* required for kernel initialization */
+#include <linux/fs.h>	/* contain filesystem structs and macros */
 
-//struct super_block sb;
-
-
+/* Initializing a file_system_type struct */
 static struct file_system_type cmpe142_fs_type = {
         .owner          = THIS_MODULE,
         .name           = "cmpe142",
@@ -19,26 +16,30 @@ static struct file_system_type cmpe142_fs_type = {
 };
 
 
+/* Initializing super block */
 int cmpe142_fill_super(struct super_block *sb, void *data, int silent);
 
+/* Initializing inode */
 struct inode *cmpe142_get_inode(struct super_block *sb, const struct inode *dir,
          umode_t mode, dev_t dev);
+
+/* required for mounting */
 extern struct dentry *cmpe142_mount(struct file_system_type *fs_type,
          int flags, const char *dev_name, void *data);
 
+/* associating file operations */
 extern const struct file_operations cmpe142_file_operations;
 
 int init_module()
 {
-	/* create the /proc file */
+	/* registering filesystem */
 	int register_fs_status = register_filesystem(&cmpe142_fs_type);
 	
-	/* check if the /proc file was created successfuly */
+	/* checking if the registration was successful */
 	if (register_fs_status != 0){
 		printk(KERN_ALERT "Error: Could not register filesystem\n");
 		return -ENOMEM;
 	}
-	
 
 	printk(KERN_INFO "Filesystem registered\n");
 
@@ -47,9 +48,9 @@ int init_module()
 
 void cleanup_module()
 {
+        /* unregistering filesystem */
 	unregister_filesystem(&cmpe142_fs_type);
 	
-	//remove_proc_entry(PROC_ENTRY_FILENAME, &proc_root);
 	printk(KERN_INFO "Filesystem removed\n");
 
 }
